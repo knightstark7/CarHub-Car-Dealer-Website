05ec51d6374bfedb4fb5e3f682359b6481df538e
Tran Nguyen Huan <93178699+knightstark7@users.noreply.github.com>
2023-12-24 23:27:42 +0700

    add allowed host

--- carhub/settings.py
+++ carhub/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = "934nw3r62@!m0^ksgw3#31tntglnr%td+-_b89xpu2@q2zqv=d"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['infinite-wave-84560-decb5b4ed4d9.herokuapp.com', 'carhubapp.com', 'www.carhubapp.com']
 
 LOGIN_REDIRECT_URL = "dashboard"
 
