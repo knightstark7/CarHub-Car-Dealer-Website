9f28ee2c57179ad42bb837dfe9358444764b8b5b
Tran Nguyen Huan <93178699+knightstark7@users.noreply.github.com>
2023-12-25 12:44:12 +0700

    Update settings.py

--- carhub/settings.py
+++ carhub/settings.py
@@ -103,9 +103,7 @@ WSGI_APPLICATION = 'carhub.wsgi.application'
 #     }
 # }
 
-DATABASES = {
-    'default': dj_database_url.config(default='postgres://postgres:new_password@localhost/carhub')
-}
+DATABASES = {'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))}
 
 
 # Password validation
