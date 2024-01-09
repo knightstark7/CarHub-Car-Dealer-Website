fc5202924c2863aee9d73931c1ce6cb39c8855ae
mingyong27 <96213465+mingyong27@users.noreply.github.com>
2024-01-09 16:40:45 +0700

    fix settings

--- carhub/settings.py
+++ carhub/settings.py
@@ -145,7 +145,7 @@ STATICFILES_DIRS = [
 
 # Media settings
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+
 
 # Messages
 from django.contrib.messages import constants as messages
